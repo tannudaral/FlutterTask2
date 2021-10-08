0e0a873f94dd2a871c1168b7b40c750b8b549992
Tannu Daral <70472388+tannudaral@users.noreply.github.com>
2021-10-08 22:28:29 +0530

    add ListView + URL launcher

--- lib/scholarship.dart
+++ lib/scholarship.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Scholarship extends StatelessWidget {
   static String id = 'scholarship';
@@ -8,43 +9,50 @@ class Scholarship extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              padding: EdgeInsets.fromLTRB(0, 20, 0, 5),
-              height: 70,
-              width: 500,
-              color: Colors.white,
-              child: Text(
-                "Scholarships",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'Aleo',
-                  fontStyle: FontStyle.normal,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30.0,
-                  color: Colors.black,
+        child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: [
+              Container(
+                // padding: EdgeInsets.fromLTRB(0, 20, 0, 5),
+                // height: 70,
+                // width: 500,
+                color: Colors.white,
+                child: Text(
+                  "Scholarships",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Aleo',
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                    color: Colors.black,
+                  ),
                 ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(2, 0, 0, 5),
-              height: 80,
-              width: 390,
-              color: Colors.white,
-              child: Text(
-                "Amazon provides talented girls scholarships worth 5 lakh rupees",
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontFamily: 'Aleo',
-                  fontStyle: FontStyle.normal,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0,
-                  color: Colors.black,
-                ),
+              SizedBox(
+                height: 25,
               ),
-            ),
+              ElevatedButton(
+                onPressed: _launchURL,
+                // Container(
+                  // padding: EdgeInsets.fromLTRB(2, 0, 0, 5),
+                  // height: 80,
+                  // width: 390,
+                  // color: Colors.white,
+                  child: Text("Amazon provides talented girls scholarships worth 5 lakh rupees",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                        fontFamily: 'Aleo',
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+              SizedBox(
+                height: 15,
+              ),
             ClipRRect(
               borderRadius: BorderRadius.circular(15.0),
               child: Image(
@@ -54,13 +62,15 @@ class Scholarship extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: 5,
+              height: 25,
             ),
-            Container(
-              padding: EdgeInsets.fromLTRB(2, 0, 0, 5),
-              height: 80,
-              width: 390,
-              color: Colors.white,
+            ElevatedButton(
+              onPressed: _launchURL,
+            // Container(
+              // padding: EdgeInsets.fromLTRB(2, 0, 0, 5),
+              // height: 80,
+              // width: 390,
+              // color: Colors.white,
               child: Text(
                 "Facebook provides scholarships worth 5 lakh rupees to school students",
                 textAlign: TextAlign.left,
@@ -73,6 +83,9 @@ class Scholarship extends StatelessWidget {
                 ),
               ),
             ),
+              SizedBox(
+                height: 15,
+              ),
             ClipRRect(
               borderRadius: BorderRadius.circular(15.0),
               child: Image(
@@ -81,9 +94,76 @@ class Scholarship extends StatelessWidget {
                 image: AssetImage('assets/scholarship.png'),
               ),
             ),
+            SizedBox(
+              height: 25,
+            ),
+            // Container(
+            //   padding: EdgeInsets.fromLTRB(2, 0, 0, 5),
+            //   height: 80,
+            //   width: 390,
+            //   color: Colors.white,
+            ElevatedButton(
+              onPressed: _launchURL,
+              child: Text(
+                "Facebook provides scholarships worth 5 lakh rupees to school students",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontFamily: 'Aleo',
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+              SizedBox(
+                height: 15,
+              ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image(
+                width: 380,
+                height: 150,
+                image: AssetImage('assets/scholarship.png'),
+              ),
+            ),
+              ElevatedButton(
+                onPressed: _launchURL,
+                child: Text(
+                  "Facebook provides scholarships worth 5 lakh rupees to school students",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontFamily: 'Aleo',
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(15.0),
+                child: Image(
+                  width: 380,
+                  height: 150,
+                  image: AssetImage('assets/scholarship.png'),
+                ),
+              ),
           ],
+          ),
         ),
-      ),
     );
+  }
+}
+
+_launchURL() async {
+  const url = 'https://time.com/5101089/jeff-bezos-daca-dreamers-scholarship-donation';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
--- lib/tech.dart
+++ lib/tech.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+const _url =
+    'https://www.nytimes.com/2021/10/01/technology/facebook-instagram-teenagers.html';
 
 class Technology extends StatelessWidget {
   static String id = 'tech';
@@ -8,8 +12,8 @@ class Technology extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+        child: ListView(
+          padding: const EdgeInsets.all(8),
           children: [
             Container(
               padding: EdgeInsets.fromLTRB(0, 30, 0, 5),
@@ -28,22 +32,31 @@ class Technology extends StatelessWidget {
                 ),
               ),
             ),
+            SizedBox(
+              height: 15,
+            ),
             Container(
               padding: EdgeInsets.fromLTRB(2, 0, 0, 5),
-              height: 100,
-              width: 400,
+              // height: 100,
+              // width: 400,
               color: Colors.white,
-              child: Text(
-                "Facebook Struggles to Quell Uproar Over Instagram’s Effect on Teens",
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontFamily: 'Aleo',
-                  fontStyle: FontStyle.normal,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0,
-                  color: Colors.black,
+              child: ElevatedButton(
+                  onPressed: _launchURL,
+                  child: Text(
+                    'Facebook Struggles to Quell Uproar Over Instagram’s Effect on Teens',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontFamily: 'Aleo',
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                      color: Colors.black,
+                    ),
+                  ),
                 ),
               ),
+            SizedBox(
+              height: 10,
             ),
             ClipRRect(
               borderRadius: BorderRadius.circular(15.0),
@@ -54,14 +67,16 @@ class Technology extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: 5,
+              height: 15,
             ),
             Container(
               padding: EdgeInsets.fromLTRB(2, 0, 0, 5),
               height: 80,
               width: 400,
               color: Colors.white,
-              child: Text(
+              child: ElevatedButton(
+                onPressed: _launchURL,
+                child: Text(
                 "Whistle-Blower to Accuse Facebook of Contributing to Jan. 6 Riot, Memo Says",
                 textAlign: TextAlign.left,
                 style: TextStyle(
@@ -72,6 +87,78 @@ class Technology extends StatelessWidget {
                   color: Colors.black,
                 ),
               ),
+            ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image(
+                width: 380,
+                height: 150,
+                image: AssetImage('assets/fbnews2.jpg'),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(2, 0, 0, 5),
+              height: 80,
+              width: 400,
+              color: Colors.white,
+              child: ElevatedButton(
+                onPressed: _launchURL,
+                child: Text(
+                  "Whistle-Blower to Accuse Facebook of Contributing to Jan. 6 Riot, Memo Says",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontFamily: 'Aleo',
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image(
+                width: 380,
+                height: 150,
+                image: AssetImage('assets/fbnews2.jpg'),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(2, 0, 0, 5),
+              height: 80,
+              width: 400,
+              color: Colors.white,
+              child: ElevatedButton(
+                onPressed: _launchURL,
+                child: Text(
+                  "Whistle-Blower to Accuse Facebook of Contributing to Jan. 6 Riot, Memo Says",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontFamily: 'Aleo',
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 15,
             ),
             ClipRRect(
               borderRadius: BorderRadius.circular(15.0),
@@ -85,5 +172,14 @@ class Technology extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+_launchURL() async {
+  const url = 'https://time.com/5101089/jeff-bezos-daca-dreamers-scholarship-donation';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
